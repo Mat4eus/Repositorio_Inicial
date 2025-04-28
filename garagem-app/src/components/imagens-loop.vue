97d3cd3196af06973601c78cbb7e6ec556ecefbf
matheus <matheushcm299@gmail.com>
2025-04-28 09:35:47 -0300

    adicao de itens

---
+++ garagem-app/src/components/imagens-loop.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="imagens-loop">
+        <div class="imagens-loop__container">
+            <div id="imagens-loop__container">
+                <img ref={{imagens}}> </img> 
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: 'ImagensLoop',
+    data() {
+        return {
+            imagens: [],
+            imagemAtual: 0,
+            intervalo: null,
+        };
+    },
+}
+</script>
