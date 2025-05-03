be2b0bc5c5705745fd09535f8aab4a7ace1fdf07
matheus <matheushcm299@gmail.com>
2025-05-02 23:31:18 -0300

    modificao-geral

--- garagem-app/src/App.vue
+++ garagem-app/src/App.vue
@@ -1,9 +1,16 @@
 <template>
-
+    <div id="app">
+        <imagens-loop/>
+    </div>
 </template>
 
 <script>
 import imagensLoop from './components/imagens-loop.vue';
-
+export default {
+    name: 'App',
+    components: {
+        imagensLoop,
+    },
+};
 
 </script>
--- garagem-app/src/components/imagens-loop.vue
+++ garagem-app/src/components/imagens-loop.vue
@@ -1,21 +1,12 @@
 <template>
-    <div class="imagens-loop">
-        <div class="imagens-loop__container">
-            <div id="imagens-loop__container">
-                <img ref={{imagens}}> </img> 
-            </div>
+    <div>
+        <div>
+            
         </div>
     </div>
 </template>
 <script>
 export default {
-    name: 'ImagensLoop',
-    data() {
-        return {
-            imagens: [],
-            imagemAtual: 0,
-            intervalo: null,
-        };
-    },
+    name: 'ImagensLoop'
 }
 </script>
